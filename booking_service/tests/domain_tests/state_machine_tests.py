bf017e219aa206c2ca37e5b767b31f3b5c0ec855
ismairdrs <ismairdrs@outlook.com>
2022-08-05 10:50:13 -0300

    test: should set status to created when reopening canceled booking

--- booking_service/tests/domain_tests/state_machine_tests.py
+++ booking_service/tests/domain_tests/state_machine_tests.py
@@ -29,3 +29,9 @@ class TestStateMachine(TestCase):
         booking.change_state(Action.PAY)
         booking.change_state(Action.REFOUND)
         self.assertEqual(booking.status, Status.REFUNDED)
+
+    def test_should_set_status_to_created_when_reopening_canceled_booking(self):
+        booking = Booking()
+        booking.status = Status.CANCELED
+        booking.change_state(Action.REOPEN)
+        self.assertEqual(booking.status, Status.CREATED)

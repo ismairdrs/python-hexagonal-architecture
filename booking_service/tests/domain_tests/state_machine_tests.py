fe0697f0eec90f91a2975d8da8aaaeaae15f03c9
ismairdrs <ismairdrs@outlook.com>
2022-08-05 10:03:37 -0300

    test: should set status to paid when  paying for booking with created status

--- booking_service/tests/domain_tests/state_machine_tests.py
+++ booking_service/tests/domain_tests/state_machine_tests.py
@@ -7,4 +7,8 @@ class TestStateMachine(TestCase):
     def test_should_always_start_with_created_status(self):
         booking = Booking()
         self.assertEqual(booking.status, Status.CREATED)
-    
+
+    def test_should_set_status_to_paid_when_paying_for_booking_with_created_status(self):
+        booking = Booking()
+        booking.change_state(Action.PAY)
+        self.assertEqual(booking.status, Status.PAID)
